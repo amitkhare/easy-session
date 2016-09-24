4ca1b1412fcbc746cffd0bd10f459c8064cfdfa8
Amit Kumar Khare <amitkhare@users.noreply.github.com>
2016-09-24 14:48:12 +0530

    Update index.php

--- example/index.php
+++ example/index.php
@@ -3,7 +3,33 @@
 // autoload via composer
 require __DIR__.'/../vendor/autoload.php';
 
-// use namespace
-use AmitKhare\EasySession;
+// OR WITHOUT COMPOSER
+// require __DIR__.'/PATH-TO/EasySession.php';
 
-$session = new EasySession();
+// Take an instance of Session Class.
+$sessionStoreName='MyCart';
+$session = new AmitKhare\EasySession($sessionStoreName);
+
+$key = 1;
+$value = ['var1'=>123,'var2'=>'amit'];
+
+// store item in $sessionStoreName
+$session->set($key,$value);
+
+// get item from $sessionStoreName by $key
+$session->get($key);
+
+// check if item exists in $sessionStoreName
+$session->exists($key);
+
+// all $sessionStoreName items
+$session->all();
+
+// remove specific item from $sessionStoreName
+$session->remove($key);
+
+// clear all items in $sessionStoreName
+$session->clear();
+
+// count items in $sessionStoreName
+$session->count();
